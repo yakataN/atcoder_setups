d7dd596ad60a5c064b3413479d37c13abad35ce8
yakataN <zeronosozaiatsume@gmail.com>
2018-07-04 01:30:57 +0900

    AtcorderExecution.batをアップしました
    AtcorderExecution.batをアップしました（まだruby専用です）

---
+++ AtcorderExecution.bat
@@ -0,0 +1,15 @@
+if exist main.rb (
+if exist sample1.txt (
+ruby main.rb < sample1.txt
+)
+if exist sample2.txt (
+ruby main.rb < sample2.txt
+)
+if exist sample3.txt (
+ruby main.rb < sample3.txt
+)
+if exist sample4.txt (
+ruby main.rb < sample4.txt
+)
+)
+pause
